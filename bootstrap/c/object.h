77d473ea243413b42131a5466ea8539d9bbcfe3a
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-08-20 16:13:38 -0600

    objects now need to know their size, and whether they're marked. This is for the garbage collector, and the size is so we can ensure we're accessing the object within the proper bounds. Max size limit of 256 MB.

--- bootstrap/c/object.h
+++ bootstrap/c/object.h
@@ -21,10 +21,15 @@
 
 typedef struct object_s
 {
-	void* slots;     // Backed by a Judy array, see judy64d.c
+	unsigned object_size:28; // How big is our object? (Max size: 256 MB)
+	unsigned reserved:3;
+	unsigned marked:1;       // Has this object been marked by the GC?
+	void* slots;             // Backed by a Judy array, see judy64d.c
 } obj_t;
 
-/* Create a new empty object. This object descends from nobody, has no slots. */
+/* Create a new empty object. This object descends from nobody, has no slots. This should probably be private, as it doesn't make
+   sense in the language, it only makes sense to set up the catch-22 linkage issue between the Object object, and the Lobby object.
+   As Lobby inherits from Object, and Object inherits from Lobby. */
 extern obj_t* obj_new_empty(void);
 
 /* Create a new object descended from a parent. This parent is installed into the newly created slot table. */
