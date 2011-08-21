3f4faaf4cf8b4e9668f946ba5f7f4c7d1579eeb0
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-08-20 18:31:20 -0600

    adds closure activate method

--- bootstrap/c/closure.c
+++ bootstrap/c/closure.c
@@ -27,5 +27,16 @@ closure_t* closure_new(obj_closure_t func)
 {
 	closure_t* closure = (closure_t*)obj_new_empty(sizeof(*closure));
 	closure->call = func;
+
+	obj_register_slot(closure, "activate", slot_new(closure, 1));
+
 	return closure;
+}
+
+/* Closure activation. Returns an obj_t because all objects share that header. Cast return to some other type if it returns for
+   instance, another closure. */
+obj_t* closure_activate(closure_t* self)
+{
+	// TODO: Add a second parameter, the environment. Also refactor the environment out of the closure_s struct.
+	return self->call(self);
 }
