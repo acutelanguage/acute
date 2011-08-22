4e5d8d1fc6f485f2f627cebac04fa4fd08329a5f
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-08-22 10:01:39 -0600

    adds to_s to Message, more message specs

--- bootstrap/rb/lib/acute/message.rb
+++ bootstrap/rb/lib/acute/message.rb
@@ -4,11 +4,35 @@
 
 module Acute
   class Message < ::Acute::Object
-    attr_accessor :name, :arguments
+    attr_accessor :name, :arguments, :next
 
     def initialize(name, arguments = [])
       @name = name
       @arguments = arguments
+      @next = nil
+    end
+
+    def to_s
+      s = ""
+      n = self
+      begin
+        s << "#{n.name}#{n.stringify_arguments} "
+      end while n = n.next
+      s.strip
+    end
+
+    protected
+
+    def stringify_arguments
+      s = ""
+      if arguments.size > 0
+        s << "("
+        arguments.each_with_index do |arg, idx|
+          s << "#{idx == 0 ? '' : ', '}#{arg}"
+        end
+        s << ")"
+      end
+      s
     end
   end
 end
--- bootstrap/rb/spec/message_spec.rb
+++ bootstrap/rb/spec/message_spec.rb
@@ -13,4 +13,29 @@ describe ::Acute::Message do
     m.name.should.eql? "test"
     m.arguments.should == [1, 2]
   end
+
+  it "changes the message name" do
+    m = ::Acute::Message.new("test")
+    m.name = "foo"
+    m.name.should.eql? "foo"
+  end
+
+  it "adds a new argument" do
+    m = ::Acute::Message.new("test", [1])
+    m.arguments << 2
+    m.arguments.should == [1, 2]
+  end
+
+  it "replaces the arguments" do
+    m = ::Acute::Message.new("test", [1, 2])
+    m.arguments = [5, 6]
+    m.arguments.should == [5, 6]
+  end
+
+  it "sets the next message" do
+    m1 = ::Acute::Message.new("testing")
+    m2 = ::Acute::Message.new("stuff", [1, 2])
+    m1.next = m2
+    m1.to_s.should.eql? "testing stuff"
+  end
 end
