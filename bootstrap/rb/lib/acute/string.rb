9b8d4fc45b0193cd8112379626d71e8c40a9b53a
Jan-Paul Bultmann <janpaulbultmann@me.com>
2011-08-30 23:33:35 +0200

    adds String asMessage to turn String into Message tree

--- bootstrap/rb/lib/acute/string.rb
+++ bootstrap/rb/lib/acute/string.rb
@@ -20,6 +20,7 @@ module Acute
       method(:prepend)   { |env| @value = env[:msg].eval_arg_at(env, 0).value + value; self }
       method(:length)    { |env| ::Acute::Number.new(@value.length) }
       method(:asString)  { |env| String.new(value.to_s) }
+      method(:asMessage) { |env| ::Acute::Parser.new.parse(value)}
     end
 
     def to_s
