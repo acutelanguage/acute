81d87824630e1527d184b21d957b71c9bb5dc76e
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-09-16 15:30:21 -0600

    adds test to ensure Slot can receive ; properly. presently it passes, and it should fail. not sure what is going on.

--- bootstrap/rb/spec/slot_spec.rb
+++ bootstrap/rb/spec/slot_spec.rb
@@ -13,4 +13,12 @@ describe ::Acute::Slot do
   it "shows a string representation of a slot" do
     ::Acute::Slot.new(42, ::Acute::Nil.instance, :activatable => true).to_s.should == "<Slot data_type=Fixnum context=nil activates>"
   end
+
+  it "receives the ; message properly" do
+    first = ::Acute::Parser.new.parse('createSlot("a", createSlot("foo", 42))')
+    tree = ::Acute::Parser.new.parse("a ; foo")
+    lobby = $state.find("Lobby")
+    lambda { lobby.perform(:target => lobby, :sender => lobby, :msg => first) }.should_not raise_error
+    lambda { lobby.perform(:target => lobby, :sender => lobby, :msg => tree) }.should_not raise_error
+  end
 end
