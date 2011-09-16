237127c0b3f1de9cf7a55f40bc4b889442539894
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-09-16 13:42:14 -0600

    getSlot can be implemented in pure Io given that we now have _lookup on Object

--- bootstrap/rb/lib/acute/object.rb
+++ bootstrap/rb/lib/acute/object.rb
@@ -19,7 +19,6 @@ module Acute
       method(:init)      { |env| env[:target] }
       method(:type)      { |env| ::Acute::String.new(env[:target].class.to_s.split("::").last) }
       method(:_lookup)   { |env| lookup(env, env[:msg].eval_arg_at(env, 0).to_s) }
-      method(:getSlot, &getSlot_method)
       method(:setSlot)   { |env| val = env[:msg].eval_arg_at(env, 1); env[:target].register(env[:msg].eval_arg_at(env, 0).to_s, val, :activatable => val.kind_of?(::Acute::Block)) }
       method(:method)    { |env, *args| ::Acute::Block.new(nil, args.pop, args) }
       method(:ruby)      { |env| env[:target].send(:eval, env[:msg].eval_arg_at(env, 0).to_s) }
@@ -86,13 +85,6 @@ module Acute
         slot = o.lookup(env, "init")
         o.perform(env.merge(:msg => ::Acute::Message.new("init"))) if slot
         o
-      end
-    end
-
-    def getSlot_method
-      lambda do |env|
-        slot = lookup(env, env[:msg].eval_arg_at(env, 0).to_s)
-        slot ? slot.data : slot
       end
     end
 
