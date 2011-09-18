b4732100d4aa5204f5d3f72b2dcc381fe060a7f8
Jan-Paul Bultmann <janpaulbultmann@me.com>
2011-09-18 02:34:13 +0200

    modifies transformer to include line and character info into messages

--- bootstrap/rb/lib/acute/transformer.rb
+++ bootstrap/rb/lib/acute/transformer.rb
@@ -21,23 +21,38 @@ module Acute
     end
     
     rule :sugarSep => simple(:sugarSep) do
-      ::Acute::Message.new(";", [])
+      lc = sugarSep.line_and_column
+      l = lc[0]
+      c = lc[1]
+      ::Acute::Message.new(";", [], {:cached_result => nil, :line_number => l, :character_number => c})
     end
 
     rule :integer => simple(:integer) do
-      ::Acute::Message.new(integer.to_s, [], { :cached_result => ::Acute::Number.new(integer.to_s.to_i) })
+      lc = integer.line_and_column
+      l = lc[0]
+      c = lc[1]
+      ::Acute::Message.new(integer.to_s, [], {:cached_result => ::Acute::Number.new(integer.to_s.to_i), :line_number => l, :character_number => c})
     end
 
     rule :string => simple(:string) do
-      ::Acute::Message.new(string.to_s, [], { :cached_result => ::Acute::String.new(string.to_s) })
+      lc = string.line_and_column
+      l = lc[0]
+      c = lc[1]-1
+      ::Acute::Message.new(string.to_s, [], {:cached_result => ::Acute::String.new(string.to_s), :line_number => l, :character_number => c})
     end
 
     rule :message => { :identifier => simple(:identifier) } do
-      ::Acute::Message.new(identifier.to_s)
+      lc = identifier.line_and_column
+      l = lc[0]
+      c = lc[1]
+      ::Acute::Message.new(identifier.to_s, [], {:cached_result => nil, :line_number => l, :character_number => c})
     end
 
-    rule :message => { :identifier => simple(:identifier), :args => subtree(:args) } do
-      ::Acute::Message.new(identifier.to_s, [*args])
+    rule :message => { :identifier => simple(:identifier), :opener => simple(:opener), :args => subtree(:args) } do
+      lc = (identifier or opener).line_and_column
+      l = lc[0]
+      c = lc[1]
+      ::Acute::Message.new(identifier.to_s, [*args], {:cached_result => nil, :line_number => l, :character_number => c})
     end
   end
 end
