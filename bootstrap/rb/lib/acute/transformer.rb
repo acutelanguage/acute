9a473808bc3eb3a647c8f2830821ccdc50c5c992
Jan-Paul Bultmann <janpaulbultmann@me.com>
2011-09-18 17:07:03 +0200

    adds origin information to transformer

--- bootstrap/rb/lib/acute/transformer.rb
+++ bootstrap/rb/lib/acute/transformer.rb
@@ -24,35 +24,35 @@ module Acute
       lc = sugar_sep.line_and_column
       l = lc[0]
       c = lc[1]
-      ::Acute::Message.new(";", [], {:cached_result => nil, :line_number => l, :character_number => c})
+      ::Acute::Message.new(";", [], {:cached_result => nil, :line_number => l, :character_number => c, :origin => origin})
     end
 
     rule :integer => simple(:integer) do
       lc = integer.line_and_column
       l = lc[0]
       c = lc[1]
-      ::Acute::Message.new(integer.to_s, [], {:cached_result => ::Acute::Number.new(integer.to_s.to_i), :line_number => l, :character_number => c})
+      ::Acute::Message.new(integer.to_s, [], {:cached_result => ::Acute::Number.new(integer.to_s.to_i), :line_number => l, :character_number => c, :origin => origin})
     end
 
     rule :string => simple(:string) do
       lc = string.line_and_column
       l = lc[0]
       c = lc[1]-1
-      ::Acute::Message.new(string.to_s, [], {:cached_result => ::Acute::String.new(string.to_s), :line_number => l, :character_number => c})
+      ::Acute::Message.new(string.to_s, [], {:cached_result => ::Acute::String.new(string.to_s), :line_number => l, :character_number => c, :origin => origin})
     end
 
     rule :message => { :identifier => simple(:identifier) } do
       lc = identifier.line_and_column
       l = lc[0]
       c = lc[1]
-      ::Acute::Message.new(identifier.to_s, [], {:cached_result => nil, :line_number => l, :character_number => c})
+      ::Acute::Message.new(identifier.to_s, [], {:cached_result => nil, :line_number => l, :character_number => c, :origin => origin})
     end
 
     rule :message => { :identifier => simple(:identifier), :opener => simple(:opener), :args => subtree(:args) } do
       lc = (identifier or opener).line_and_column
       l = lc[0]
       c = lc[1]
-      ::Acute::Message.new(identifier.to_s, [*args], {:cached_result => nil, :line_number => l, :character_number => c})
+      ::Acute::Message.new(identifier.to_s, [*args], {:cached_result => nil, :line_number => l, :character_number => c, :origin => origin})
     end
   end
 end
