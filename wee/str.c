9883fe3f141112d26d86d9db1476e4e25b626fd6
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-05-16 18:40:27 -0600

    adds primitive string

---
+++ wee/str.c
@@ -0,0 +1,43 @@
+/*
+ * Acute Programming Language
+ * Copyright (c) 2011, Jeremy Tregunna, All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "str.h"
+
+str_t* str_new(char* string, size_t length)
+{
+    str_t* str = (str_t*)obj_new_with_size(sizeof(*str));
+    memcpy(str->data, string, length);
+    str->length = length;
+    return str;
+}
+
+void str_destroy(str_t* str)
+{
+    if(str->data)
+        free(str->data);
+    free(str);
+    str = NULL;
+}
